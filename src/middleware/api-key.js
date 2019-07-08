31a744c2bd6febdcbf2418c7a991d63c43ac1188
Kate Beard <asouthernbellein@gmail.com>
2019-07-08 11:33:35 +0100

    Update api key header  🐿 v2.12.4

--- src/middleware/api-key.js
+++ src/middleware/api-key.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = (req, res, next) => {
-	if (req.headers.apikey === process.env.APIKEY) {
+	if (req.headers['x-api-key'] === process.env.APIKEY) {
 		next();
 	} else {
 		res.status(401).json('You need an API key, soz');
